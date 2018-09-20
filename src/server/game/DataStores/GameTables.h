9a3dfa2cab0b41017e50f95139cecc2a527d77a1
Dave [Laptop] <david@ideatica.cl>
2018-09-20 18:30:17 -0300

    fix possible crash

--- src/server/game/DataStores/GameTables.h
+++ src/server/game/DataStores/GameTables.h
@@ -265,48 +265,50 @@ inline float GetGameTableColumnForClass(T const* row, int32 class_)
 
 inline float GetSpellScalingColumnForClass(GtSpellScalingEntry const* row, int32 class_)
 {
-    switch (class_)
+    if(row)
     {
-        case CLASS_WARRIOR:
-            return row->Warrior;
-        case CLASS_PALADIN:
-            return row->Paladin;
-        case CLASS_HUNTER:
-            return row->Hunter;
-        case CLASS_ROGUE:
-            return row->Rogue;
-        case CLASS_PRIEST:
-            return row->Priest;
-        case CLASS_DEATH_KNIGHT:
-            return row->DeathKnight;
-        case CLASS_SHAMAN:
-            return row->Shaman;
-        case CLASS_MAGE:
-            return row->Mage;
-        case CLASS_WARLOCK:
-            return row->Warlock;
-        case CLASS_MONK:
-            return row->Monk;
-        case CLASS_DRUID:
-            return row->Druid;
-        case CLASS_DEMON_HUNTER:
-            return row->DemonHunter;
-        case -1:
-            return row->Item;
-        case -2:
-            return row->Consumable;
-        case -3:
-            return row->Gem1;
-        case -4:
-            return row->Gem2;
-        case -5:
-            return row->Gem3;
-        case -6:
-            return row->Health;
-        default:
-            break;
+        switch (class_)
+        {
+            case CLASS_WARRIOR:
+                return row->Warrior;
+            case CLASS_PALADIN:
+                return row->Paladin;
+            case CLASS_HUNTER:
+                return row->Hunter;
+            case CLASS_ROGUE:
+                return row->Rogue;
+            case CLASS_PRIEST:
+                return row->Priest;
+            case CLASS_DEATH_KNIGHT:
+                return row->DeathKnight;
+            case CLASS_SHAMAN:
+                return row->Shaman;
+            case CLASS_MAGE:
+                return row->Mage;
+            case CLASS_WARLOCK:
+                return row->Warlock;
+            case CLASS_MONK:
+                return row->Monk;
+            case CLASS_DRUID:
+                return row->Druid;
+            case CLASS_DEMON_HUNTER:
+                return row->DemonHunter;
+            case -1:
+                return row->Item;
+            case -2:
+                return row->Consumable;
+            case -3:
+                return row->Gem1;
+            case -4:
+                return row->Gem2;
+            case -5:
+                return row->Gem3;
+            case -6:
+                return row->Health;
+            default:
+                break;
+        }
     }
-
     return 0.0f;
 }
 
